445c433c73d9224c2d66a5f0905eb4a5ee416027
Grenish rai <mrcoder2033d@gmail.com>
2026-06-10 19:59:54 +0530

    Add ChefWord and Menu sections to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import ChefWord from "@/components/chef-word";
 import HomePage from "@/components/home-page";
+import Menu from "@/components/menu";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
+      <ChefWord />
+      <Menu />
     </div>
   );
 }
