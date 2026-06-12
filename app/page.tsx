e968600911e1e9ae61f98d9847778827c4e892cf
Grenish rai <mrcoder2033d@gmail.com>
2026-06-12 17:36:00 +0530

    Add Gallery, Location, Reservations, and Testimonials sections to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import ChefWord from "@/components/chef-word";
+import Gallery from "@/components/gallery";
 import HomePage from "@/components/home-page";
+import Location from "@/components/location";
 import Menu from "@/components/menu";
+import Reservations from "@/components/reservations";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -8,6 +12,10 @@ export default function Home() {
       <HomePage />
       <ChefWord />
       <Menu />
+      <Testimonials />
+      <Gallery />
+      <Reservations />
+      <Location />
     </div>
   );
 }
